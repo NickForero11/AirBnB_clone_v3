ec61fec8111e7a37ea2dfed73fe6ee95e0484295
juanquinnterodev <896@holbertonschool.com>
2020-01-31 02:51:09 +0000

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,15 @@
-"""Module to handle the views of the REST API using blueprints.
-
-Variables:
-    app_views (Blueprint): blueprint for the endpoints of the REST API.
+#!/usr/bin/python3
+"""Module init
 """
-
-
 from flask import Blueprint
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
