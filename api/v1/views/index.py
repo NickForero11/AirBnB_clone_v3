228b6aebac674567871b1cdc1d0df3cc1011c46e
juanquinnterodev <896@holbertonschool.com>
2020-01-31 01:28:06 +0000

    number types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -10,3 +11,16 @@ def status():
     """Handler function for the status endpoint.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def count():
+    """retrieves the number of each objects by type
+    """
+    numtype = {"amenities": storage.count("Amenity"),
+                  "cities": storage.count("City"),
+                  "places": storage.count("Place"),
+                  "reviews": storage.count("Review"),
+                  "states": storage.count("State"),
+                  "users": storage.count("User")}
+    return jsonify(numtype)
