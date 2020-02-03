a11a35d3f57200fe77beb1a84d68113f632809c6
NickForero11 <nicolasforero1199@gmail.com>
2020-02-03 01:40:28 -0500

    Fixed missing import on place_reviews view

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
+from models.place import Place
 
 
 @app_views.route("/places/<place_id>/reviews", methods=['GET', 'POST'],
